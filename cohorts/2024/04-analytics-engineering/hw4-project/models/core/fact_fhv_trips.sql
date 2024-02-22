1d5141830789ee96ac00d08384058862ac468127
hadwyn.yin@gmail.com <hadwyn.yin@gmail.com>
2024-02-22 07:48:15 +0000

    DataTalk Week 4 Homework - update

--- cohorts/2024/04-analytics-engineering/hw4-project/models/core/fact_fhv_trips.sql
+++ cohorts/2024/04-analytics-engineering/hw4-project/models/core/fact_fhv_trips.sql
@@ -1,10 +1,10 @@
 {{ config(materialized="table") }}
 
 with
-    fhv_tripdata as (select * from {{ ref("stg_fhv_tripdate") }}),
+    fhv_tripdata as (select * from {{ ref("stg_fhv_tripdata") }}),
     dim_zones as (select * from {{ ref("dim_zones") }} where borough != 'Unknown')
 select
-    fhv_tripdata.tripid,
+    -- fhv_tripdata.tripid,
     fhv_tripdata.dispatching_base_num,
     fhv_tripdata.pickup_locationid,
     pickup_zone.borough as pickup_borough,
@@ -17,11 +17,14 @@ select
     fhv_tripdata.sr_flag,
     fhv_tripdata.affiliated_base_number
 from fhv_tripdata
-inner join
+left join
     dim_zones as pickup_zone on fhv_tripdata.pickup_locationid = pickup_zone.locationid
-inner join
+left join
     dim_zones as dropoff_zone
     on fhv_tripdata.dropoff_locationid = dropoff_zone.locationid
 where
-    fhv_tripdata.pickup_datetime > cast('2018-12-31 12:59:59 UTC' as timestamp)
-    and fhv_tripdata.pickup_datetime < cast('2020-01-01 00:00:00 UTC' as timestamp)
+    fhv_tripdata.pickup_datetime > cast('2018-12-31 12:59:59 UTC' as timestamp) AND 
+    fhv_tripdata.pickup_datetime < cast('2020-01-01 00:00:00 UTC' as timestamp) AND
+    pickup_zone.borough is not null AND
+    dropoff_zone.borough is not null
+
--- cohorts/2024/04-analytics-engineering/hw4-project/models/staging/dev/stg_green_tripdata.sql
+++ cohorts/2024/04-analytics-engineering/hw4-project/models/staging/dev/stg_green_tripdata.sql
@@ -45,8 +45,8 @@ where rn = 1
 
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
+-- {% if var('is_test_run', default=true) %}
 
-  limit 100
+--   limit 100
 
-{% endif %}
+-- {% endif %}
